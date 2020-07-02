30068467ed64ea96ec8bfa1236321f6d00aec151
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-02 10:24:15 -0500

    changing the amount of repos

--- src/Components/Repositories/Repositories.js
+++ src/Components/Repositories/Repositories.js
@@ -14,7 +14,7 @@ const Repositories = () => {
         if(data) {
             repos = JSON.parse(data)
             setRepoCount(repos.length)
-            repos = repos.slice(14, 20)
+            repos = repos.slice(8, 14)
             return setRepo(repos)
         } 
 
@@ -23,7 +23,7 @@ const Repositories = () => {
             repos = await response.json()
             setRepoCount(repos.length)
             sessionStorage.setItem('repo', JSON.stringify(repos))
-            repos = repos.slice(14, 20)
+            repos = repos.slice(8, 14)
             setRepo(repos)
         }
 
